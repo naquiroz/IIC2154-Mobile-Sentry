73e959383dc6a996adcfb1b76a5b83aa0d65989f
Nicolas <naquiroz@uc.cl>
2021-08-30 11:42:36 -0400

    Add example env dev

---
+++ app/config/example.env.dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  SENTRY_DSN: "your sentry dsn",
+  SENTRY_ORG: "your sentry org name",
+  SENTRY_PROJECT: "sentry project name",
+  SENTRY_AUTH_TOKEN: "your sentry authorization token"
+}
