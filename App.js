c2ef80eeac0a086d32f99ef523d8c7a34349802a
Nicolas <naquiroz@uc.cl>
2021-08-30 11:41:03 -0400

    Add sentry example

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
 // This is the first file that ReactNative will run when it starts up.
 import App from "./app/app.tsx"
 import { registerRootComponent } from "expo"
+import * as Sentry from 'sentry-expo';
+import env from "./app/config/env";
+Sentry.init({
+  dsn: env.SENTRY_DSN,
+  enableInExpoDevelopment: true,
+  debug: true, // Sentry will try to print out useful debugging information if something goes wrong with sending an event. Set this to `false` in production.
+});
+
 
 registerRootComponent(App)
 export default App
--- app/screens/demo/demo-screen.tsx
+++ app/screens/demo/demo-screen.tsx
@@ -97,45 +97,12 @@ export const DemoScreen: FC<StackScreenProps<NavigatorParamList, "demo">> = obse
   ({ navigation }) => {
     const goBack = () => navigation.goBack()
 
-    const demoReactotron = React.useMemo(
-      () => async () => {
-        console.tron.log("Your Friendly tron log message")
-        console.tron.logImportant("I am important")
-        console.tron.display({
-          name: "DISPLAY",
-          value: {
-            numbers: 1,
-            strings: "strings",
-            booleans: true,
-            arrays: [1, 2, 3],
-            objects: {
-              deeper: {
-                deeper: {
-                  yay: "👾",
-                },
-              },
-            },
-            functionNames: function hello() {
-              /* dummy function */
-            },
-          },
-          preview: "More control with display()",
-          important: true,
-          image: {
-            uri:
-              "https://avatars2.githubusercontent.com/u/3902527?s=200&u=a0d16b13ed719f35d95ca0f4440f5d07c32c349a&v=4",
-          },
-        })
-        // make an API call for the demo
-        // Don't do API like this, use store's API
-        const demo = new Api()
-        demo.setup()
-        demo.getUser("1")
-        // Let's do some async storage stuff
-        await save("Cool Name", "Boaty McBoatface")
-      },
-      [],
-    )
+    const raiseSentryError = () => {
+      console.log('====================================');
+      console.log("Logging error");
+      console.log('====================================');
+      throw new Error("AAAA SOMETHING HAPPENED")
+    };
 
     return (
       <View testID="DemoScreen" style={FULL}>
@@ -160,7 +127,7 @@ export const DemoScreen: FC<StackScreenProps<NavigatorParamList, "demo">> = obse
               style={DEMO}
               textStyle={DEMO_TEXT}
               tx="demoScreen.reactotron"
-              onPress={demoReactotron}
+              onPress={raiseSentryError}
             />
             <Text style={HINT} tx={`demoScreen.${Platform.OS}ReactotronHint` as const} />
           </View>
